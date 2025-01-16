9a609de2927577a218f8197498a535ccd6538ee7
elvanisa <elvanisamuhsina@gmail.com>
2025-01-16 19:40:16 +0700

    cypress POM

--- cypress/e2e/web-test/saucedemo.cy.js
+++ cypress/e2e/web-test/saucedemo.cy.js
@@ -1,16 +1,17 @@
+import saucedemoPage from "../../support/pageObject/saucedemoPage"
+
 describe('Saucedemo Login Scenarios', () => {
   beforeEach(() => {
     cy.visit('')
   })
   it('login success', () => {
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    cy.loginSaucedemo('standard_user','secret_sauce')
     cy.get('[data-test="title"]').should('contain.text','Products')
   })
-  it('login failed - empty username', () => {
+  it.only('login failed - empty username', () => {
     cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    //cy.get('#login-button').click()
+    saucedemoPage.clickLogin()
     cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required')
   })
   it('login failed - empty password', () => {
@@ -19,9 +20,15 @@ describe('Saucedemo Login Scenarios', () => {
     cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Password is required')
   })
   it('login failed - locked user', () => {
-    cy.get('#user-name').type('locked_out_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
-    cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
+    cy.loginSaucedemo('locked_out_user','secret_sauce')
+    cy.VerifyError('Epic sadface: Sorry, this user has been locked out.')
+    //cy.get('#user-name').type('locked_out_user')
+    //cy.get('#password').type('secret_sauce')
+    //cy.get('#login-button').click()
+    //cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Sorry, this user has been locked out.')
   })
+  it.skip('login env', () => {
+    cy.get('#user-name').type(Cypress.env('userprod'))
+    cy.get('#password').type('secret_sauce')
+    cy.get('#ogin-button').click()})
 })
